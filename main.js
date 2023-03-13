51a1d1a8c387273d77362870aed8d349e0f9edef
Diwakar kumar <diwakarkumar3096@gmail.com>
2023-03-13 17:30:42 +0530

    todto list

---
+++ main.js
@@ -0,0 +1,9 @@
+let inputvalue=document.getElementById('inputbox').value;
+let input=document.getElementById('inputbox');
+let addTodo=document.getElementById('addTodo');
+
+function myFunction(){
+    addTodo.style.display="block";
+}
+  
+      
